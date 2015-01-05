9b8a39bbf2e66196102127de8c410bfc3647c030
Pavel Kalatski <ext-pavel.kalatski@elisa.fi>
2015-01-05 17:38:07 +0200

    Riddle is now read from file.

--- src/main/scala/net/pahona/dragonage/riddle/Main.scala
+++ src/main/scala/net/pahona/dragonage/riddle/Main.scala
@@ -1,13 +1,13 @@
 package net.pahona.dragonage.riddle
 
-object Main extends App {
-  //  val riddle = Set((1, 2), (1, 3), (2, 3), (4, 2), (5, 3), (2, 6), (3, 6))
-//  val riddle = Set((1,2), (1,4), (2,4), (3,4), (3,6), (6,4), (4,5), (4,7), (5,7), (4,8), (4,9), (4,12), (8,9),
-//    (10,8), (10,12), (8,12), (9,12), (9,11), (12,11), (12,13), (12,14), (12,15), (12,16), (13,16), (14,15), (15,16),
-//    (16,17), (15,18), (17,18))
-  val riddle = Set((1,3), (1,4), (2,4), (2,5), (3,4), (4,5), (4,6), (4,7), (4,8), (4,9), (6,8), (6,10), (7,10), (7,9),
-    (8,11), (8,10), (9,10), (9,12), (10,13), (11,13), (10,14), (12,14))
+import java.nio.file.{Paths, Files}
 
+import net.pahona.dragonage.riddle.Solver.Riddle
+
+import scala.collection.JavaConversions._
+
+object Main extends App {
+  val riddle = readRiddle()
 
   val solution = Solver.solve(riddle)
 
@@ -19,4 +19,13 @@ object Main extends App {
   }
 
   println("DONE")
+
+  private[this] def readRiddle(): Riddle = {
+    Files.readAllLines(Paths.get("riddle.txt")) map parseLine toSet
+  }
+
+  private[this] def parseLine(lineWithDash: String) = {
+    val arr = lineWithDash.split('-')
+    (arr(0).toInt, arr(1).toInt)
+  }
 }
